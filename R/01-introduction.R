cf34e6c77c3e01426d7dc944cf186bd577238c7d
avleon <avlapodaca@gmail.com>
2020-03-24 12:23:51 -0600

    This are my notes

--- R/01-introduction.R
+++ R/01-introduction.R
@@ -4,3 +4,65 @@
 
 ## Notes
 
+## ----'quick_intro_01', message = FALSE-------------------------------------------------------------------------------
+library('scRNAseq')
+library('scater')
+library('scran')
+library('plotly')
+
+
+## ----'quick_intro_02', cache = TRUE----------------------------------------------------------------------------------
+sce <- scRNAseq::MacoskoRetinaData()
+
+## How big is the data?
+pryr::object_size(sce)
+
+## How does it look?
+sce
+
+## ----'quick_intro_03', cache = TRUE----------------------------------------------------------------------------------
+# Quality control.
+is.mito <- grepl("^MT-", rownames(sce))
+qcstats <-
+    scater::perCellQCMetrics(sce, subsets = list(Mito = is.mito))
+filtered <-
+    scater::quickPerCellQC(qcstats, percent_subsets = "subsets_Mito_percent")
+sce <- sce[, !filtered$discard]
+
+# Normalization.
+sce <- scater::logNormCounts(sce)
+
+# Feature selection.
+dec <- scran::modelGeneVar(sce)
+hvg <- scran::getTopHVGs(dec, prop = 0.1)
+
+# Dimensionality reduction.
+set.seed(1234)
+sce <- scater::runPCA(sce, ncomponents = 25, subset_row = hvg)
+sce <- scater::runUMAP(sce, dimred = 'PCA', external_neighbors = TRUE)
+
+# Clustering.
+g <- scran::buildSNNGraph(sce, use.dimred = 'PCA')
+sce$clusters <- factor(igraph::cluster_louvain(g)$membership)
+
+
+## ----'quick_intro_04'------------------------------------------------------------------------------------------------
+# Visualization.
+scater::plotUMAP(sce, colour_by = "clusters")
+
+
+scater::plotUMAP(sce, colour_by = "clusters")
+
+## # If I'm working through the cluster connection, then I need to
+
+pdf('plot_umap.pdf')
+scater::plotUMAP(sce, colour_by = "clusters")
+
+dev.off()
+
+## ----'quick_intro_05', eval = FALSE----------------------------------------------------------------------------------
+## # Interactive visualization
+
+# This makes the map interactive, showing little notes
+p <- scater::plotUMAP(sce, colour_by = "clusters")
+plotly::ggplotly(p)
